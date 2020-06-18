6b1c2684a1d48ee867e1a060d9adeb3e49bc9835
tinker-michaelj <michael.tinker@hedera.com>
2020-06-18 13:42:37 -0500

    Some stuff

---
+++ hedera-node/src/main/java/com/hedera/services/state/merkle/Blob.java
@@ -0,0 +1,239 @@
+package com.hedera.services.state.merkle;
+
+/*-
+ * ‌
+ * Hedera Services Node
+ * ​
+ * Copyright (C) 2018 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import com.google.common.base.MoreObjects;
+import com.swirlds.blob.BinaryObject;
+import com.swirlds.blob.BinaryObjectStore;
+import com.swirlds.common.FCMValue;
+import com.swirlds.common.FastCopyable;
+import com.swirlds.common.crypto.Hash;
+import com.swirlds.common.io.SerializableDataInputStream;
+import com.swirlds.common.io.SerializableDataOutputStream;
+import com.swirlds.common.io.SerializedObjectProvider;
+import com.swirlds.common.merkle.MerkleExternalLeaf;
+import com.swirlds.common.merkle.utility.AbstractMerkleNode;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Blob extends AbstractMerkleNode implements FCMValue, MerkleExternalLeaf {
+	static final int MERKLE_VERSION = (int)BinaryObject.CLASS_VERSION;
+	static final long RUNTIME_CONSTRUCTABLE_ID = 0x4cefb15eb131d9e3L;
+	static final Hash MISSING_DELEGATE_HASH = new Hash(new byte[] {
+			(byte)0x00, (byte)0x01, (byte)0x02, (byte)0x03,
+			(byte)0x04, (byte)0x05, (byte)0x06, (byte)0x07,
+			(byte)0x08, (byte)0x09, (byte)0x0a, (byte)0x0b,
+			(byte)0x0c, (byte)0x0d, (byte)0x0e, (byte)0x0f,
+			(byte)0x10, (byte)0x11, (byte)0x12, (byte)0x13,
+			(byte)0x14, (byte)0x15, (byte)0x16, (byte)0x17,
+			(byte)0x18, (byte)0x19, (byte)0x1a, (byte)0x1b,
+			(byte)0x1c, (byte)0x1d, (byte)0x1e, (byte)0x1f,
+			(byte)0x20, (byte)0x21, (byte)0x22, (byte)0x23,
+			(byte)0x24, (byte)0x25, (byte)0x26, (byte)0x27,
+			(byte)0x28, (byte)0x29, (byte)0x2a, (byte)0x2b,
+			(byte)0x2c, (byte)0x2d, (byte)0x2e, (byte)0x2f,
+			(byte)0x30, (byte)0x31, (byte)0x32, (byte)0x33,
+			(byte)0x34, (byte)0x35, (byte)0x36, (byte)0x37,
+			(byte)0x38, (byte)0x39, (byte)0x3a, (byte)0x3b,
+			(byte)0x3c, (byte)0x3d, (byte)0x3e, (byte)0x3f,
+	});
+	static final byte[] NO_DATA = new byte[0];
+	static final BinaryObject MISSING_DELEGATE = null;
+
+	private BinaryObject delegate = MISSING_DELEGATE;
+
+	public Blob() { }
+
+	public Blob(byte[] data) {
+		delegate = BinaryObjectStore.getInstance().put(data);
+	}
+
+	public Blob(BinaryObject delegate) {
+		this.delegate = delegate;
+	}
+
+	@Deprecated
+	public static class Provider implements SerializedObjectProvider {
+		@Override
+		public FastCopyable deserialize(DataInputStream _in) throws IOException {
+			var blob = new Blob();
+			var in = (SerializableDataInputStream)_in;
+
+			var hasData = in.readBoolean();
+			if (hasData) {
+				var delegate = new BinaryObject();
+				delegate.copyFrom(in);
+				delegate.copyFromExtra(in);
+				blob.setDelegate(delegate);
+			}
+
+			return blob;
+		}
+	}
+
+	/* --- MerkleExternalLeaf --- */
+	@Override
+	public long getClassId() {
+		return RUNTIME_CONSTRUCTABLE_ID;
+	}
+
+	@Override
+	public int getVersion() {
+		return MERKLE_VERSION;
+	}
+
+	@Override
+	public Hash getHash() {
+		return (delegate == MISSING_DELEGATE) ? MISSING_DELEGATE_HASH : delegate.getHash();
+	}
+
+	@Override
+	public void serialize(SerializableDataOutputStream out) throws IOException {
+		if (delegate == MISSING_DELEGATE) {
+			out.writeBoolean(false);
+		} else {
+			out.writeBoolean(true);
+			delegate.copyTo(out);
+			delegate.copyToExtra(out);
+		}
+	}
+
+	@Override
+	public void deserialize(SerializableDataInputStream in, int version) throws IOException {
+		var hasData = in.readBoolean();
+		if (hasData) {
+			delegate = new BinaryObject();
+			delegate.copyFrom(in);
+			delegate.copyFromExtra(in);
+		}
+	}
+
+	@Override
+	public void serializeAbbreviated(SerializableDataOutputStream out) {
+	}
+
+	@Override
+	public void deserializeAbbreviated(
+			SerializableDataInputStream in,
+			Hash hash,
+			int version
+	) {
+		if (!MISSING_DELEGATE_HASH.equals(hash)) {
+			delegate = new BinaryObject();
+			delegate.deserializeAbbreviated(in, hash, version);
+		}
+	}
+
+	/* --- FastCopyable --- */
+	@Override
+	public Blob copy() {
+		return new Blob(delegate.copy());
+	}
+
+	@Override
+	public void delete() {
+		if (delegate != MISSING_DELEGATE) {
+			delegate.delete();
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || Blob.class != o.getClass()) {
+			return false;
+		}
+
+		var that = (Blob)o;
+
+		return Objects.equals(this.delegate, that.delegate);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Objects.hashCode(delegate));
+	}
+
+	@Override
+	@Deprecated
+	public void copyTo(SerializableDataOutputStream out) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void copyToExtra(SerializableDataOutputStream out) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	@Deprecated
+	public void copyFrom(SerializableDataInputStream in) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	@Deprecated
+	public void copyFromExtra(SerializableDataInputStream in) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void diffCopyTo(SerializableDataOutputStream out, SerializableDataInputStream in) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void diffCopyFrom(SerializableDataOutputStream out, SerializableDataInputStream in) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* --- Bean --- */
+	public BinaryObject getDelegate() {
+		return delegate;
+	}
+
+	public void setDelegate(BinaryObject delegate) {
+		this.delegate = delegate;
+	}
+
+	public byte[] getData() {
+		return (delegate == MISSING_DELEGATE) ? NO_DATA : BinaryObjectStore.getInstance().get(delegate);
+	}
+
+	public void setData(byte[] data) {
+		if (delegate == MISSING_DELEGATE) {
+			delegate = BinaryObjectStore.getInstance().put(data);
+		} else {
+			delegate = BinaryObjectStore.getInstance().update(delegate, data);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this)
+				.add("delegate", delegate)
+				.toString();
+	}
+}
--- hedera-node/src/main/java/com/hedera/services/state/merkle/BlobPath.java
+++ hedera-node/src/main/java/com/hedera/services/state/merkle/BlobPath.java
@@ -9,9 +9,9 @@ package com.hedera.services.state.merkle;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,9 +21,6 @@ package com.hedera.services.state.merkle;
  */
 
 import com.google.common.base.MoreObjects;
-import com.hederahashgraph.api.proto.java.AccountID;
-import com.hederahashgraph.api.proto.java.ContractID;
-import com.hederahashgraph.api.proto.java.TopicID;
 import com.swirlds.common.FCMKey;
 import com.swirlds.common.FastCopyable;
 import com.swirlds.common.io.SerializableDataInputStream;
@@ -34,50 +31,37 @@ import com.swirlds.common.merkle.utility.AbstractMerkleNode;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Objects;
 
-public class EntityId extends AbstractMerkleNode implements FCMKey, MerkleLeaf {
+import static com.swirlds.common.CommonUtils.getNormalisedStringBytes;
+
+public class BlobPath extends AbstractMerkleNode implements FCMKey, MerkleLeaf {
 	static final int MERKLE_VERSION = 1;
-	static final long RUNTIME_CONSTRUCTABLE_ID = 0xd5dd2ebaa0bde03L;
+	static final long RUNTIME_CONSTRUCTABLE_ID = 0x9c19df177063b4caL;
 
-	private long shardNum;
-	private long realmNum;
-	private long idNum;
+	public static final int MAX_PATH_LEN = 4_096;
 
-	public EntityId() { }
+	private String literal;
 
-	public EntityId(long shardNum, long realmNum, long idNum) {
-		this.shardNum = shardNum;
-		this.realmNum = realmNum;
-		this.idNum = idNum;
-	}
+	public BlobPath() { }
 
-	public static EntityId fromPojoAccount(AccountID pojo) {
-		return new EntityId(pojo.getShardNum(), pojo.getRealmNum(), pojo.getAccountNum());
-	}
-
-	public static EntityId fromPojoTopic(TopicID pojo) {
-		return new EntityId(pojo.getShardNum(), pojo.getRealmNum(), pojo.getTopicNum());
-	}
-
-	public static EntityId fromPojoContract(ContractID pojo) {
-		return new EntityId(pojo.getShardNum(), pojo.getRealmNum(), pojo.getContractNum());
+	public BlobPath(String literal) {
+		this.literal = literal;
 	}
 
 	@Deprecated
 	public static class Provider implements SerializedObjectProvider {
 		@Override
-		public FastCopyable deserialize(DataInputStream in) throws IOException {
-			var id = new EntityId();
+		public FastCopyable deserialize(DataInputStream _in) throws IOException {
+			var path = new BlobPath();
+			var in = (SerializableDataInputStream)_in;
 
 			in.readLong();
 			in.readLong();
 
-			id.realmNum = in.readLong();
-			id.shardNum = in.readLong();
-			id.idNum = in.readLong();
-
-			return id;
+			path.setLiteral(in.readNormalisedString(MAX_PATH_LEN));
+			return path;
 		}
 	}
 
@@ -93,23 +77,19 @@ public class EntityId extends AbstractMerkleNode implements FCMKey, MerkleLeaf {
 	}
 
 	@Override
-	public void deserialize(SerializableDataInputStream in, int version) throws IOException {
-		shardNum = in.readLong();
-		realmNum = in.readLong();
-		idNum = in.readLong();
+	public void serialize(SerializableDataOutputStream out) throws IOException {
+		out.writeNormalisedString(literal);
 	}
 
 	@Override
-	public void serialize(SerializableDataOutputStream out) throws IOException {
-		out.writeLong(shardNum);
-		out.writeLong(realmNum);
-		out.writeLong(idNum);
+	public void deserialize(SerializableDataInputStream in, int version) throws IOException {
+		literal = in.readNormalisedString(MAX_PATH_LEN);
 	}
 
 	/* --- FastCopyable --- */
 	@Override
-	public EntityId copy() {
-		return new EntityId(shardNum, realmNum, idNum);
+	public BlobPath copy() {
+		return new BlobPath(literal);
 	}
 
 	@Override
@@ -120,17 +100,18 @@ public class EntityId extends AbstractMerkleNode implements FCMKey, MerkleLeaf {
 		if (this == o) {
 			return true;
 		}
-		if (o == null || EntityId.class != o.getClass()) {
+		if (o == null || BlobPath.class != o.getClass()) {
 			return false;
 		}
 
-		var that = (EntityId)o;
-		return shardNum == that.shardNum && realmNum == that.realmNum && idNum == that.idNum;
+		var that = (BlobPath)o;
+
+		return Objects.equals(this.literal, that.literal);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(shardNum, realmNum, idNum);
+		return Arrays.hashCode(getNormalisedStringBytes(literal));
 	}
 
 	@Override
@@ -167,36 +148,18 @@ public class EntityId extends AbstractMerkleNode implements FCMKey, MerkleLeaf {
 	}
 
 	/* --- Bean --- */
-	public long getShardNum() {
-		return shardNum;
+	public String getLiteral() {
+		return literal;
 	}
 
-	public void setShardNum(final long shardNum) {
-		this.shardNum = shardNum;
-	}
-
-	public long getRealmNum() {
-		return realmNum;
-	}
-
-	public void setRealmNum(final long realmNum) {
-		this.realmNum = realmNum;
-	}
-
-	public long getIdNum() {
-		return idNum;
-	}
-
-	public void setIdNum(final long idNum) {
-		this.idNum = idNum;
+	public void setLiteral(String literal) {
+		this.literal = literal;
 	}
 
 	@Override
 	public String toString() {
 		return MoreObjects.toStringHelper(this)
-				.add("shard", shardNum)
-				.add("realm", realmNum)
-				.add("entity", idNum)
+				.add("literal", literal)
 				.toString();
 	}
 }
